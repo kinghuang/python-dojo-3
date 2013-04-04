b8dbc86c292512706a6162cde5d304a8d6eec0e1
King Chung Huang <kinghuang@mac.com>
2013-04-03 20:55:32 -0600

    Added editing forms for existing tasks.
    Existing tasks can be set to 'complete'.

--- djangotodo/todolist/forms.py
+++ djangotodo/todolist/forms.py
@@ -1,6 +1,19 @@
+from django import forms
 from django.forms import ModelForm
+from django.forms.models import modelformset_factory
 from models import Task
 
+class NewTaskForm(ModelForm):
+	description = forms.CharField()
+
+	class Meta:
+		model = Task	
+		exclude = ('completed',)
+
 class TaskForm(ModelForm):
+	description = forms.CharField()
+
 	class Meta:
 		model = Task
+
+TaskFormSet = modelformset_factory(Task, extra=0, form=TaskForm)
--- djangotodo/todolist/views.py
+++ djangotodo/todolist/views.py
@@ -1,22 +1,30 @@
 from django.shortcuts import render
 from todolist.models import Task
-from forms import TaskForm
+from forms import NewTaskForm, TaskForm, TaskFormSet
 from django.core.exceptions import PermissionDenied
 
 def list(request):
-	if request.method == "GET":
-		form = TaskForm()
-	elif request.method == "POST":
-		form = TaskForm(request.POST)
-		if form.is_valid():
-			form.save()
-			# make sure to return use a clean form for the resulting page
-			form = TaskForm()
+	form = NewTaskForm()
+	formset = TaskFormSet()
+	
+	if request.method == "POST":
+		if (request.POST['button'] == 'Update!'):
+			formset = TaskFormSet(request.POST)
+			if formset.is_valid():
+				formset.save()
+				formset = TaskFormSet()
+		else:
+			form = NewTaskForm(request.POST)
+			if form.is_valid():
+				form.save()
+				# make sure to return use a clean form for the resulting page
+				form = NewTaskForm()
 
-	else:
+	elif request.method != 'GET':
 		raise PermissionDenied('Only GET and POST methods are allowed')
 
 	return render(request, 'todolist/list-view.html', {
     	'tasks': Task.objects.all(),
-    	'form': form
+    	'form': form,
+    	'formset': formset
     })
