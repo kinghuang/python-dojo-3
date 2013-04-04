2c7b9e9bb08172483ab9064ccee79dfe27f70f9e
King Chung Huang <kinghuang@mac.com>
2013-04-03 19:40:41 -0600

    Added a “completed” field to Task.

--- djangotodo/todolist/models.py
+++ djangotodo/todolist/models.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 class Task(models.Model):
     description = models.TextField(max_length=200)
+    completed = models.BooleanField()
 
     def __unicode__(self):
     	return self.description
